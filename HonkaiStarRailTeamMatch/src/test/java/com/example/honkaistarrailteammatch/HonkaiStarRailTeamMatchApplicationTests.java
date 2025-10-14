213b10a7231cc032d3f63731545422ac57697df2
Nneif <nneifemakpunonu@gmail.com>
2025-10-14 11:18:43 -0400

    added folders in the java folder

---
+++ HonkaiStarRailTeamMatch/src/test/java/com/example/honkaistarrailteammatch/HonkaiStarRailTeamMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honkaistarrailteammatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonkaiStarRailTeamMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
